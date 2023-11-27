61ee7c54823a0b41a8632bb37a3f267f75845dfb
Yuhang Zhou <zyhvg77@gmail.com>
2023-11-27 18:50:12 +0800

    finish first parallel version

--- src/cacheline_visit.cpp
+++ src/cacheline_visit.cpp
@@ -2,6 +2,7 @@
 #include "builder.h"
 #include "bfs.h"
 #include "utils.h"
+#include <cstring>
 #include "plf_nanotimer.h"
 #include <omp.h>
 #include <filesystem>
@@ -21,10 +22,10 @@ int main(int argc, char *argv[]) {
 
     fs::path graph_file_path(GRAPH_FILE_PREFIX);
 
-    std::array<std::string, 1> graph_files{
-//        "rmat_17.txt",
-//        "rmat_18.txt",
-//        "rmat_19.txt",
+    std::vector<std::string> graph_files{
+        "rmat_17.txt",
+        "rmat_18.txt",
+        "rmat_19.txt",
         "rmat_20.txt"
     };
 
@@ -39,36 +40,40 @@ int main(int argc, char *argv[]) {
         auto [reordered, new_ids, new_ids_remap] = reorder_by_degree(graph);
         std::clog << "Graph Reorder: " << timer.get_elapsed_ms() << " ms" << std::endl;
 
-        timer.start();
-//        int block_size = 65536;
-        int block_size = 8;
+        std::cout << graph.get_vertex_number() << std::endl;
 
-        long double edge_vis{};
+        timer.start();
+        int block_size = 65536;
+
         std::vector<long double> vertex_edge_visit(graph.get_vertex_number());
         std::vector<long double> vertex_edge_visit_cacheline(graph.get_vertex_number());
-
         std::vector<long double> reorder_vertex_edge_visit(graph.get_vertex_number());
         std::vector<long double> reorder_vertex_edge_visit_cacheline(graph.get_vertex_number());
 
         // backup data when every block finished
-        for (int block_id{}; block_id < 64 / block_size; ++block_id) {
+        for (int block_id{}; block_id < graph.get_vertex_number() / block_size; ++block_id) {
+            std::fill(vertex_edge_visit.begin(), vertex_edge_visit.end(), 0.0);
+            std::fill(vertex_edge_visit_cacheline.begin(), vertex_edge_visit_cacheline.end(), 0.0);
+            std::fill(reorder_vertex_edge_visit.begin(), reorder_vertex_edge_visit.end(), 0.0);
+            std::fill(reorder_vertex_edge_visit_cacheline.begin(), reorder_vertex_edge_visit_cacheline.end(), 0.0);
+
             Node vid_beg = block_id * block_size;
             Node vid_end = std::min((block_id + 1) * block_size, static_cast<Node>(graph.get_vertex_number()));
-            std::cout << std::format("Start to process block {} ({}-{})", block_id, vid_beg, vid_end) << std::endl;
+            std::cout << std::format("Start to process block {} ({}-{})", block_id, vid_beg, vid_end - 1) << std::endl;
             #pragma omp parallel default(none) shared(graph, vid_beg, vid_end, vertex_edge_visit, vertex_edge_visit_cacheline)
             {
                 std::vector<long double> l_vertex_edge_visit(graph.get_vertex_number());
                 std::vector<long double> l_vertex_edge_visit_cacheline(graph.get_vertex_number());
                 Memory<unsigned> memory{graph};
-                #pragma omp for schedule(dynamic, 1) nowait
+                #pragma omp for schedule(dynamic, 1024) nowait
                 for (Node vid = vid_beg; vid < vid_end; ++vid) {
-//                    if (graph.out_degree(vid) > 0) {
+                    if (graph.out_degree(vid) > 0) {
                         auto [t_visit, t_visit_cacheline] = do_cacheline_bfs(graph, vid, memory);
                         std::transform(l_vertex_edge_visit.begin(), l_vertex_edge_visit.end(),
                                        t_visit.begin(), l_vertex_edge_visit.begin(), std::plus<>());
                         std::transform(l_vertex_edge_visit_cacheline.begin(), l_vertex_edge_visit_cacheline.end(),
                                        t_visit_cacheline.begin(), l_vertex_edge_visit_cacheline.begin(), std::plus<>());
-//                    }
+                    }
                 }
                 #pragma omp critical
                 {
@@ -84,15 +89,15 @@ int main(int argc, char *argv[]) {
                 std::vector<long double> l_vertex_edge_visit(reordered.get_vertex_number());
                 std::vector<long double> l_vertex_edge_visit_cacheline(reordered.get_vertex_number());
                 Memory<unsigned> memory{reordered};
-                #pragma omp for schedule(dynamic, 1) nowait
+                #pragma omp for schedule(dynamic, 1024) nowait
                 for (Node vid = vid_beg; vid < vid_end; ++vid) {
-//                    if (reordered.out_degree(new_ids[vid]) > 0) {
+                    if (reordered.out_degree(new_ids[vid]) > 0) {
                         auto [t_visit, t_visit_cacheline] = do_cacheline_bfs(reordered, new_ids[vid], memory);
                         std::transform(l_vertex_edge_visit.begin(), l_vertex_edge_visit.end(),
                                        t_visit.begin(), l_vertex_edge_visit.begin(), std::plus<>());
                         std::transform(l_vertex_edge_visit_cacheline.begin(), l_vertex_edge_visit_cacheline.end(),
                                        t_visit_cacheline.begin(), l_vertex_edge_visit_cacheline.begin(), std::plus<>());
-//                    }
+                    }
                 }
                 #pragma omp critical
                 {
@@ -104,10 +109,10 @@ int main(int argc, char *argv[]) {
             }
 
             std::cout << "Block " << block_id << " Finish!" << std::endl;
-            reduce_backup(vertex_edge_visit, graph_file + "vertex_edge_visit.bin", std::plus<>());
-            reduce_backup(vertex_edge_visit_cacheline, graph_file + "vertex_edge_visit_cacheline.bin", std::plus<>());
-            reduce_backup(reorder_vertex_edge_visit, graph_file + "reorder_vertex_edge_visit.bin", std::plus<>());
-            reduce_backup(reorder_vertex_edge_visit_cacheline, graph_file + "reorder_vertex_edge_visit_cacheline.bin", std::plus<>());
+            reduce_backup(vertex_edge_visit, graph_file + "-vertex_edge_visit.bin", std::plus<>());
+            reduce_backup(vertex_edge_visit_cacheline, graph_file + "-vertex_edge_visit_cacheline.bin", std::plus<>());
+            reduce_backup(reorder_vertex_edge_visit, graph_file + "-reorder_vertex_edge_visit.bin", std::plus<>());
+            reduce_backup(reorder_vertex_edge_visit_cacheline, graph_file + "-reorder_vertex_edge_visit_cacheline.bin", std::plus<>());
             std::cout << "Backup Finish!" << std::endl;
         }
     }
--- src/test_cpp.cpp
+++ src/test_cpp.cpp
@@ -6,15 +6,15 @@
 #include <iostream>
 
 int main(int argc, char *argv[]) {
-    static long double buf[1048576];
+    static long double buf[131071];
 
     std::ifstream in;
-    in.open("rmat_20.txtreorder_vertex_edge_visit_cacheline.bin", std::ios::in | std::ios::binary);
-    in.read(reinterpret_cast<char*>(buf), sizeof(long double) * 1048576);
+    in.open("rmat_17.txt-vertex_edge_visit.bin", std::ios::in | std::ios::binary);
+    in.read(reinterpret_cast<char*>(buf), sizeof(long double) * 131071);
     in.close();
 
     long double sum = std::reduce(std::begin(buf), std::end(buf), 0.0, std::plus<>());
-    std::cout << (sum / 1048576 / 64) << std::endl;
+    std::cout << (sum / 131071 / 64) << std::endl;
 
     return 0;
 }
