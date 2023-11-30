c31b0b6bce29bdcf18ee3d6197b26a4baf15530c
Yuhang Zhou <zyhvg77@gmail.com>
2023-11-30 11:26:50 +0800

    update

--- src/misc.cpp
+++ src/misc.cpp
@@ -178,18 +178,20 @@ std::vector<PropT> pull_eb_active_num_ana(Graph<T, DstT> const &graph, T root) {
     return depth;
 }
 
-//int main(int argc, char *argv[]) {
-//    fs::path graph_file_path(DATASET_PATH);
-//    if (argc < 2) {
-//        graph_file_path /= "rmat_20.txt";
-//    } else {
-//        graph_file_path /= argv[1];
-//    }
-//
-//    Builder<Node> builder{graph_file_path.string(), true};
-//    Graph<Node> graph = builder.build_csr();
-//    std::clog << "Graph: " << graph_file_path.string() << std::endl;
-//
+int main(int argc, char *argv[]) {
+    fs::path graph_file_path(DATASET_PATH);
+    if (argc < 2) {
+        graph_file_path /= "rmat_17.txt";
+    } else {
+        graph_file_path /= argv[1];
+    }
+
+    Builder<Node> builder{graph_file_path.string(), true};
+    Graph<Node> graph = builder.build_csr();
+    std::clog << "Graph: " << graph_file_path.string() << std::endl;
+
+    std::cout << graph.out_degree(114280)  << " " << graph.in_degree(114280) << std::endl;
+
 //    Graph<Node> simplified = simplify_graph(graph);
 //
 //    std::ofstream out("undirected-soc-Livejournal1.txt", std::ios::out | std::ios::trunc);
@@ -203,11 +205,11 @@ std::vector<PropT> pull_eb_active_num_ana(Graph<T, DstT> const &graph, T root) {
 //    }
 //    out.flush();
 //    out.close();
-//
-//    return 0;
-//}
 
-int main(int argc, char *argv[]) {
+    return 0;
+}
+
+int _main(int argc, char *argv[]) {
     fs::path graph_file_path(DATASET_PATH);
     if (argc < 2) {
         graph_file_path /= "rmat_20.txt";
