fd53aefc0e45a25c1aa8d152c73abb85e9ce8136
Yuhang Zhou <zyhvg77@gmail.com>
2023-12-07 18:04:27 +0800

    update

--- src/parent_stats.cpp
+++ src/parent_stats.cpp
@@ -13,58 +13,77 @@ using Node = int;
 using Prop = int;
 
 constexpr int block_size = 65536;
+constexpr int vertex_offset = 327680;
 
 int main(int argc, char *argv[]) {
-    omp_set_num_threads(4); // set to an appropriate number according to the CPU cores
+    omp_set_num_threads(80); // set to an appropriate number according to the CPU cores
 
     plf::nanotimer timer;
 
     fs::path graph_file_path(DATASET_PATH);
 
     std::vector<std::string> graph_files{
-       "rmat_17.txt",
-       "rmat_18.txt",
-       "rmat_19.txt",
-       "rmat_20.txt"
+            // "rmat_18.txt",
+            // "rmat_19.txt",
+            // "rmat_20.txt",
+            // "soc-Slashdot0811.txt",
+            // "soc-Slashdot0902.txt",
+            // "soc-LiveJournal1.txt",
+            // "com-orkut.ungraph.txt",
+            "soc-pokec-relationships.txt"
+    };
+    std::vector<std::string> directed{
+            "soc-Slashdot0811.txt",
+            "soc-Slashdot0902.txt",
+            "soc-LiveJournal1.txt",
+            "soc-pokec-relationships.txt"
     };
 
-    for (auto const &graph_file : graph_files) {
+    for (auto const &graph_file: graph_files) {
         std::clog << "Starting to process " << graph_file << std::endl;
         std::clog << "Dataset path: " << (graph_file_path / graph_file).string() << std::endl;
+        bool is_directed_graph = (std::find(directed.begin(), directed.end(), graph_file) != directed.end());
+        std::clog << "Directed/Need Symmetrize: " << is_directed_graph << std::endl;
         timer.start();
-        Builder<Node> builder{(graph_file_path / graph_file).string()};
+        Builder<Node> builder{(graph_file_path / graph_file).string(), is_directed_graph};
         Graph<Node> graph = builder.build_csr();
         std::clog << "Graph Construction: " << timer.get_elapsed_ms() << " ms" << std::endl;
 
-        int non_iso_count{};
-        Node vertex_to_go{};
-        for (Node i : std::views::iota(0, graph.get_vertex_number())) {
-            vertex_to_go++;
-            if (graph.out_degree(i) > 0) {
-                non_iso_count++;
-                if (non_iso_count == graph.get_vertex_number()/200)
-                    break;
-            }
-        }
+        // int non_iso_count{};
+        // Node vertex_to_go{};
+        // for (Node i : std::views::iota(0, graph.get_vertex_number())) {
+        //     vertex_to_go++;
+        //     if (graph.out_degree(i) > 0) {
+        //         non_iso_count++;
+        //         if (non_iso_count == graph.get_vertex_number()/200)
+        //             break;
+        //     }
+        // }
 
-        std::cout << "Vertex To Go: " << vertex_to_go << std::endl;
-        std::cout << "Non Isolated Number: " << non_iso_count << " " << graph.get_vertex_number()/200 << std::endl;
+        // std::cout << "Vertex To Go: " << vertex_to_go << std::endl;
+        // std::cout << "Non Isolated Number: " << non_iso_count << " " << graph.get_vertex_number()/200 << std::endl;
 
         std::vector<long double> parent_cnt(graph.get_vertex_number());
 
+        int vertex_to_go = graph.get_vertex_number() - vertex_offset;
+
+        std::cout << std::format("Vertex to go: {}", vertex_to_go) << std::endl;
         int block_number = (vertex_to_go + block_size - 1) / block_size;
+        std::cout << std::format("Block Number: {}", block_number) << std::endl;
         // backup data when every block finishes
         for (int block_id{}; block_id < block_number; ++block_id) {
             std::fill(parent_cnt.begin(), parent_cnt.end(), 0.0);
 
-            Node vid_beg = block_id * block_size;
-            Node vid_end = std::min((block_id + 1) * block_size, vertex_to_go);
+            Node vid_beg = vertex_offset + block_id * block_size;
+            Node vid_end = std::min(vertex_offset + (block_id + 1) * block_size, vertex_offset + vertex_to_go);
             std::cout << std::format("Start to process block {} ({}-{})", block_id, vid_beg, vid_end - 1) << std::endl;
+
             timer.start();
             #pragma omp parallel default(none) shared(graph, vid_beg, vid_end, parent_cnt)
             {
                 std::vector<long double> l_parent_cnt(graph.get_vertex_number());
-                #pragma omp for schedule(dynamic, 1024) nowait
+                // #pragma omp for schedule(dynamic, 1024) nowait
+                #pragma omp for nowait
                 for (Node vid = vid_beg; vid < vid_end; ++vid) {
                     if (graph.out_degree(vid) > 0) {
                         std::vector<Prop> depth = do_bfs(graph, vid);
@@ -82,7 +101,7 @@ int main(int argc, char *argv[]) {
                         }
                     }
                 }
-                #pragma omp critical
+#pragma omp critical
                 {
                     std::transform(parent_cnt.begin(), parent_cnt.end(),
                                    l_parent_cnt.begin(), parent_cnt.begin(), std::plus<>());
--- src/test_cpp.cpp
+++ src/test_cpp.cpp
@@ -5,6 +5,9 @@
 #include <numeric>
 #include <iostream>
 #include <filesystem>
+#include <ranges>
+#include <execution>
+#include <format>
 
 namespace fs = std::filesystem;
 
@@ -19,18 +22,29 @@ void get_block_num(T vertex_num, Types ...vertex_nums) {
 }
 
 int main(int argc, char *argv[]) {
-    static long double buf[131071];
+    std::string file_name{"/home/aozora/TempData/soc-pokec-relationships.txt-parent_cnt.bin"};
 
-    fs::path record_path(RECORD_PATH);
-    std::string graph_file{"rmat_17.txt"};
-
-    std::ifstream in;
-    in.open((record_path/(graph_file + "-reorder_vertex_edge_visit.bin")).string(), std::ios::in | std::ios::binary);
-    in.read(reinterpret_cast<char*>(buf), sizeof(long double) * 131071);
+    std::ifstream in(file_name, std::ios::in | std::ios::binary);
+    if (!in.is_open()) {
+        std::cout << "ERROR OPEN FILE" << std::endl;
+        exit(-1);
+    }
+    in.seekg(0, std::ios::end);
+    std::streampos sz = in.tellg();
+    in.seekg(0, std::ios::beg);
+    size_t vertex_number = sz / sizeof(long double);
+    std::vector<long double> freq(vertex_number);
+    in.read(reinterpret_cast<char*>(freq.data()), sz);
     in.close();
 
-    long double sum = std::reduce(std::begin(buf), std::end(buf), 0.0, std::plus<>());
-    std::cout << (sum / 131071 / 131071) << std::endl;
-
+    auto max_elem = std::ranges::max_element(freq);
+    std::cout << std::format("Max: {}", *max_elem) << std::endl;
+    std::cout << std::format("Max Unsigned Long Long: {}", std::numeric_limits<unsigned long long>::max()) << std::endl;
+    bool valid = std::transform_reduce(std::execution::par, freq.begin(), freq.end(), true,
+                        std::logical_and<>(),
+                        [](long double const &f) {
+                            return f >= 0.0;
+                        });
+    std::cout << "Valid: " << valid << std::endl;
     return 0;
 }
